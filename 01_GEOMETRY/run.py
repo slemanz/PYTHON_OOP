30940849a042687b77ae7a1ccd7bcbeac345163f
slemanz <william_sleman@hotmail.com>
2024-04-26 05:16:59 -0300

    01(feat): add rectangle class

--- 01_GEOMETRY/run.py
+++ 01_GEOMETRY/run.py
@@ -4,9 +4,9 @@ class Point:
         self.x = x
         self.y = y
 
-    def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0] < self.x < upright[0] \
-                and lowleft[1] < self.y < upright[1]:
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x < self.x < rectangle.upright.x \
+                and rectangle.lowleft.y < self.y < rectangle.upright.y:
             return True
         else:
             return False
@@ -14,8 +14,19 @@ class Point:
     def distance_from_point(self, point):
         return ((self.x - point.x)**2 + (self.y - point.y)**2)**0.5
 
-point1 = Point(1,1)
+class Rectangle:
 
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
+
+
+point1 = Point(1,1)
 point2 = Point(3,3)
 
 print(point1.distance_from_point(point2))
+
+rect = Rectangle(point1, point2)
+
+point3 = Point(2,2)
+print(point3.falls_in_rectangle(rect))
