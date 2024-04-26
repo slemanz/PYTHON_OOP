97bf514b0952032c4371036ece6348af10413122
slemanz <william_sleman@hotmail.com>
2024-04-26 04:54:09 -0300

    01(feat): add first method

--- 01_GEOMETRY/run.py
+++ 01_GEOMETRY/run.py
@@ -4,4 +4,12 @@ class Point:
         self.x = x
         self.y = y
 
-point1 = Point(10,20)
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] \
+                and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
+
+point1 = Point(6,8)
+print(point1.falls_in_rectangle((5,6), (7,9)))
