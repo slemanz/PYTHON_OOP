31a3707fbaeb4bc4a3d291c362addd794fd6e863
slemanz <william_sleman@hotmail.com>
2024-04-28 17:05:51 -0300

    01(feat): add gui rectangle child

--- 01_GEOMETRY/run.py
+++ 01_GEOMETRY/run.py
@@ -1,3 +1,7 @@
+from random import randint
+import turtle
+
+
 class Point:
 
     def __init__(self, x, y):
@@ -14,33 +18,59 @@ class Point:
     def distance_from_point(self, point):
         return ((self.x - point.x)**2 + (self.y - point.y)**2)**0.5
 
+
+
 class Rectangle:
 
-    def __init__(self, lowleft, upright):
-        self.lowleft = lowleft
-        self.upright = upright
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
 
     def area(self):
-        return (self.upright.x -self.lowleft.x) * (self.upright.y - self.lowleft.y)
+        return (self.point2.x -self.point1.x) * (self.point2.y - self.point1.y)
 
 
-from random import randint
+class GuiRectangle(Rectangle):
 
-rectangle = Rectangle(
-    Point(randint(0,9), randint(0,9)),
-    Point(randint(10,19), randint(10,19))
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.point1.x, self.point1.y)
+
+        canvas.pendown()
+        canvas.forward(100)
+
+
+        turtle.done()
+
+gui_rectangle = GuiRectangle(
+    Point(randint(0,400), randint(0,400)),
+    Point(randint(10,400), randint(10,400))
 )
 
+myturtle = turtle.Turtle()
+gui_rectangle.draw(canvas=myturtle)
+
+
+
+'''
+# create rectangle object
+rectangle = Rectangle(
+    Point(randint(0,400), randint(0,400)),
+    Point(randint(10,400), randint(10,400))
+)
+
+# Print rectangle coordinates
 print("Rectangle coordinates: ",
         rectangle.lowleft.x, ",",
         rectangle.lowleft.y, "and",
         rectangle.upright.x, ",",
         rectangle.upright.y)
 
+# get point and area from user
 user_point = Point(float(input("Guess X: ")), float(input("Guess Y: ")))
-
 user_area = float(input("Guess rectangle area: "))
 
+# Print out the game result
 print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
-
 print("Your area was:", rectangle.area())
+'''
