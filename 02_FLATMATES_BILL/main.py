64d48a0dc100568f79207aa9cbe132dbf18a4e9b
slemanz <william_sleman@hotmail.com>
2024-04-30 04:20:17 -0300

    02(feat): add method pays

--- 02_FLATMATES_BILL/main.py
+++ 02_FLATMATES_BILL/main.py
@@ -19,8 +19,10 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        return bill.amount / 2
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
@@ -41,4 +43,5 @@ bill = Bill(amount=120, period="March 2023")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
-print(john.pays(bill=bill))
+print("John pays: {:.2f}".format(john.pays(bill=bill, flatmate2=marry)))
+print("Marry pays: {:.2f}".format(marry.pays(bill=bill, flatmate2=john)))
