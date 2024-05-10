ed8ac9097e3c3b1fdea8b7e3b6ce6a6117b1ad00
slemanz <william_sleman@hotmail.com>
2024-05-10 04:26:26 -0300

    03(test): drawing shapes

--- 03_MATH_PAINTER/main.py
+++ 03_MATH_PAINTER/main.py
@@ -2,8 +2,14 @@ import numpy as np
 from PIL import Image
 
 # Create 3d numpy array of zeros, then replace zeros (black, pixels) with yellow pixels
-data = np.zeros((1000, 1000, 3), dtype=np.uint8)
+data = np.zeros((100, 100, 3), dtype=np.uint8)
 data[:] = [255,255, 0]
+
+# Make a red patch
+data[20:60, 20:60] = [255, 0, 0]
+data[20:60, 20:60] = [255, 0, 0]
+
+data[70:80, 70:80] = [0, 255, 0]
 
 img = Image.fromarray(data, 'RGB')
 img.save('canvas.png')
