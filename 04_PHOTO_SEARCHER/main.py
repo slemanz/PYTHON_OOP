0b0c0aa24047e2e6b6a8e5b3c0418207171f2a5d
slemanz <william_sleman@hotmail.com>
2024-05-12 12:24:41 -0300

    04(feat): add first classes structures

--- 04_PHOTO_SEARCHER/main.py
+++ 04_PHOTO_SEARCHER/main.py
@@ -1,2 +1,22 @@
-# test
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+
+class FirstScreen(Screen):
+
+    def search_image(self):
+        pass
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
+
+print("Ok")
