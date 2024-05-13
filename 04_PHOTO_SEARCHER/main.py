e3ea1624139e693c0ac176283a0a8f8428ee7fed
slemanz <william_sleman@hotmail.com>
2024-05-13 03:33:55 -0300

    04(feat): add frontend initial code

--- 04_PHOTO_SEARCHER/main.py
+++ 04_PHOTO_SEARCHER/main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+
+Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
