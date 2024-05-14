0ef3faea6c47a42d45bfea263c91bead8fa8c9ac
slemanz <william_sleman@hotmail.com>
2024-05-14 03:47:53 -0300

    04(test): test the wikipedia and request libs

--- 04_PHOTO_SEARCHER/main.py
+++ 04_PHOTO_SEARCHER/main.py
@@ -1,8 +1,15 @@
+'''
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+'''
+
+import wikipedia
+import requests
 
 
+
+'''
 Builder.load_file('frontend.kv')
 
 
@@ -23,4 +30,15 @@ class MainApp(App):
 
 MainApp().run()
 
+'''
 print("Ok")
+
+page = wikipedia.page("Onepiece")
+
+link = page.images[0]
+print(link)
+
+req = requests.get(link)
+
+with open("one_piece.jpg", 'wb') as file:
+    file.write(req._content)
