4d64bf3370a0003521d4f714607da91f3fded62f
slemanz <william_sleman@hotmail.com>
2024-05-15 03:48:06 -0300

    04(feat): add text search, but not working yet

--- 04_PHOTO_SEARCHER/main.py
+++ 04_PHOTO_SEARCHER/main.py
@@ -1,22 +1,41 @@
-'''
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-'''
 
 import wikipedia
 import requests
 
 
 
-'''
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
 
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/luffy.jpg'
+        # get user query from textinput
+        query = self.manager.current_screen.ids.user_query.text
+
+        # get wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+
+        # Download the image
+        req = requests.get(image_link)
+        print(image_link)
+        imagepath = 'files/image.jpg'
+
+        print(req._content)
+
+        """
+        with open(imagepath, 'wb') as file:
+            file.write(req._content)
+        """
+
+
+        # set the image in the image widget
+        self.manager.current_screen.ids.img.source = imagepath
+
 
 class RootWidget(ScreenManager):
     pass
@@ -30,15 +49,3 @@ class MainApp(App):
 
 MainApp().run()
 
-'''
-print("Ok")
-
-page = wikipedia.page("Onepiece")
-
-link = page.images[0]
-print(link)
-
-req = requests.get(link)
-
-with open("one_piece.jpg", 'wb') as file:
-    file.write(req._content)
