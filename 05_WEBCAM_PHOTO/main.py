ae6ee12e4aef91da30d104fcbd16c33e20c371d2
slemanz <william_sleman@hotmail.com>
2024-05-21 03:24:11 -0300

    05(feat): add copy feature

--- 05_WEBCAM_PHOTO/main.py
+++ 05_WEBCAM_PHOTO/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
 import time
 
 from filesave import FileSave
@@ -15,16 +16,20 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
+        '''Starts camera and changes Button text'''
         self.ids.camera.play = True
         self.ids.camera_button.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        '''Stops camera and changes Button text'''
         self.ids.camera.play = False
         self.ids.camera_button.text = 'Start Camera'
         self.ids.camera.texture = None
 
     def capture(self):
+        '''Creates a filename with the current time and captures
+        and saves a photo image under that filename'''
         current_time = time.strftime("%Y%m%d-%H%M%S")
         filepath = "files/image_"+ current_time +".png"
         self.ids.camera.export_to_png(filepath)
@@ -34,6 +39,17 @@ class CameraScreen(Screen):
 
 
 class ImageScreen(Screen):
+    def create_link(self):
+        print("Not implemented")
+        self.url = 'Not implemented'
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        try:
+            Clipboard.copy(self.url)
+        except:
+            print("Error")
+
     def back(self):
         self.manager.current = 'camera_screen'
 
