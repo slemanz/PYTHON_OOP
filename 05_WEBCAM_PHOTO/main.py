c918c95d4719ff1d4a54cee4b8087923a5c2102b
slemanz <william_sleman@hotmail.com>
2024-05-22 03:18:55 -0300

    05(feat): add background color

--- 05_WEBCAM_PHOTO/main.py
+++ 05_WEBCAM_PHOTO/main.py
@@ -20,12 +20,15 @@ class CameraScreen(Screen):
         self.ids.camera.play = True
         self.ids.camera_button.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
+        self.ids.camera.opacity = 1
+
 
     def stop(self):
         '''Stops camera and changes Button text'''
         self.ids.camera.play = False
         self.ids.camera_button.text = 'Start Camera'
         self.ids.camera.texture = None
+        self.ids.camera.opacity = 0
 
     def capture(self):
         '''Creates a filename with the current time and captures
